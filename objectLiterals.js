658003f1f39a055e25518d54da79888494268397
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-23 22:41:51 +0800

    objectLiterals - Computed Property Names

--- objectLiterals.js
+++ objectLiterals.js
@@ -1,3 +1,5 @@
+// 1. Property Value Shorthands
+/* 
 var name = 'han-aureole';
 var email = 'hanaureole@gmail.com';
 var age = 25;
@@ -6,4 +8,29 @@ var user = { name, email, age };
 console.log(user);
 
 var user = { userName: name, email, age };
-console.log(user);
+console.log(user); 
+*/
+
+// 2. Computed Property Names
+// before
+/*
+function objectify(key, value) {
+  let obj = {};
+  obj[key] = value;
+  return obj;
+}
+
+const ts1 = objectify('name', 'Tyler');
+console.log(ts1);
+*/
+
+// after
+/*
+function objectify(key, value) {
+  // return { [key]: value };
+  return { [key]: value };
+}
+
+const ts2 = objectify('name', 'Aureole');
+console.log(ts2);
+*/
