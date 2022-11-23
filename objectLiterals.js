d0ca6af57409e044349f79da4f56a2e36af6e912
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-23 22:13:58 +0800

    objectLiterals - Property value Shorthands

---
+++ objectLiterals.js
@@ -0,0 +1,9 @@
+var name = 'han-aureole';
+var email = 'hanaureole@gmail.com';
+var age = 25;
+
+var user = { name, email, age };
+console.log(user);
+
+var user = { userName: name, email, age };
+console.log(user);
